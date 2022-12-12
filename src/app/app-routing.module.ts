7075ba6b137fde3f7ceddc618bd1a01a245255df
Devesh Mukherjee <deveshmukherjee76@gmail.com>
2022-12-12 23:24:38 +0530

    frontend final

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TaskviewComponent } from './pages/taskview/taskview.component';
+
+const routes: Routes = [
+  { path: "", component: TaskviewComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/pages/taskview/taskview.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-taskview',
+  templateUrl: './taskview.component.html',
+  styleUrls: ['./taskview.component.scss']
+})
+export class TaskviewComponent {
+
+}
